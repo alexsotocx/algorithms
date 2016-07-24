4600ed7358478a53a7fe0b57e60be41be7d2788a
Alexander Soto Cardona <asoto@innventto.com>
2016-07-23 21:02:50 -0500

    SRM 232 DIV 2 - Easy | Medium - Reformated

--- Topcoder/SRM 232 DIV 2/src/BritishCoins.java
+++ Topcoder/SRM 232 DIV 2/src/BritishCoins.java
@@ -2,8 +2,8 @@
 public class BritishCoins {
 
   public int[] coins(int pence) {
-		int pounds = pence / (12*20);
-    pence -= pounds * (12*20);
+    int pounds = pence / (12 * 20);
+    pence -= pounds * (12 * 20);
     int shillings = pence / 12;
     pence -= shillings * 12;
     return new int[]{pounds, shillings, pence};
