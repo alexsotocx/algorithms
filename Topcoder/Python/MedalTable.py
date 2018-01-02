2314bc72ccacf5a038bb3463e04f5543517eb624
Alexander Soto <alexsotocx@gmail.com>
2018-01-02 20:49:06 +0100

    Save

--- Topcoder/Python/MedalTable.py
+++ Topcoder/Python/MedalTable.py
@@ -1,23 +1,23 @@
 class MedalTable:
   results = {}
   def initiliaze_results(self, result):
-    if result not in results:
-      results[result] = [0, 0, 0]
+    if result not in self.results:
+      self.results[result] = [0, 0, 0]
   def inte(self, ints):
-    return(ints * -1)
+    return(str(ints * -1))
   def generate(self, results):
     results_split = [ result.split() for result in results ]
-    print(results_split)
     for result in results_split:
       i = 0
-      for cat in results:
+      for cat in result:
         self.initiliaze_results(cat)
-        results[cat][i] -= 1
+        self.results[cat][i] -= 1
         i += 1
     self.results = [(tuple(val), key) for key, val in self.results.iteritems()  ]
-    print(results)
-    results.sort()
+    self.results.sort()
     ans = []
-    for result in results:
-      ans.append(' '.join([result[1]] + map(inte, results[0])))
+    for result in self.results:
+      ans.append(' '.join([result[1]] + map(self.inte, result[0])))
     return(ans)
+
+MedalTable().generate(x)
--- Topcoder/Python/Quipu.py
+++ Topcoder/Python/Quipu.py
@@ -1,9 +1,10 @@
+import re
+
 class Quipu:
   def readKnots(self, knots):
-    knots = knots.split("(-X*)")
-
-    knots = knots[:len(knots)]
+    knots = re.findall("(-X*)", knots)
+    knots = knots[:len(knots)-1]
     number = []
     for knot in knots:
-      number.append(len(knot)-1)
+      number.append(str(len(knot)-1))
     return int(''.join(number))
