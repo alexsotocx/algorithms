48e7f5de3410751f2622f16749a494eeac0b31a8
Alexander Soto <alexsotocx@gmail.com>
2018-05-16 22:40:59 +0200

    Insert Binary tree

--- DataStructures - Golang/implementations/data/structures/trees/binary_tree.go
+++ DataStructures - Golang/implementations/data/structures/trees/binary_tree.go
@@ -1,7 +1,5 @@
 package trees
 
-import "errors"
-
 type node struct {
   LeftChildren *node
   RightChildren *node
@@ -19,27 +17,64 @@ type BinaryTree struct {
   size uint
 }
 
-func (tree *BinaryTree) insert(data interface{})  {
+func (tree *BinaryTree) Insert(data interface{})  {
   if tree.IsEmpty() {
     tree.root = newNode(data)
   } else {
     node := newNode(data)
     parentNode := tree.root
-    for i := uint(1); i <= tree.size; i *= 2 {
-      if (tree.size & i) != 1 {
+    for i := uint(1); i <= tree.size; i <<= 2 {
+      if (tree.size & i) != 0 {
         if parentNode.LeftChildren == nil {
           parentNode.LeftChildren = node
-          return
+          break
         }
         parentNode = parentNode.LeftChildren
       } else {
         if parentNode.RightChildren == nil {
           parentNode.RightChildren = node
-          return
+          break
         }
         parentNode = parentNode.RightChildren
       }
     }
+  }
+  tree.size++
+}
+
+func (tree BinaryTree) InOrder(eachFunction func(interface{})) {
+  tree.inOrder(tree.root, eachFunction)
+}
+
+func (tree BinaryTree) inOrder(current *node, eachFunction func(interface{})) {
+  if current != nil {
+    tree.inOrder(current.LeftChildren, eachFunction)
+    eachFunction(current.Data)
+    tree.inOrder(current.RightChildren, eachFunction)
+  }
+}
+
+func (tree BinaryTree) PreOrder(eachFunction func(interface{})) {
+  tree.preOrder(tree.root, eachFunction)
+}
+
+func (tree BinaryTree) preOrder(current *node, eachFunction func(interface{})) {
+  if current != nil {
+    eachFunction(current.Data)
+    tree.inOrder(current.LeftChildren, eachFunction)
+    tree.inOrder(current.RightChildren, eachFunction)
+  }
+}
+
+func (tree BinaryTree) PosOrder(eachFunction func(interface{})) {
+  tree.posOrder(tree.root, eachFunction)
+}
+
+func (tree BinaryTree) posOrder(current *node, eachFunction func(interface{})) {
+  if current != nil {
+    tree.inOrder(current.LeftChildren, eachFunction)
+    tree.inOrder(current.RightChildren, eachFunction)
+    eachFunction(current.Data)
   }
 }
 
@@ -47,6 +82,9 @@ func (tree BinaryTree) IsEmpty() bool {
   return tree.size == 0
 }
 
+func (tree BinaryTree) Size() uint {
+  return tree.size
+}
 
 
 
---
+++ DataStructures - Golang/implementations/test/trees/binary_tree_test.go
@@ -0,0 +1,54 @@
+package trees_test
+
+import(
+  "testing"
+  "../../data/structures/trees"
+)
+
+func TestInsert(t *testing.T) {
+  tree := trees.BinaryTree{}
+  tree.Insert(1)
+  tree.Insert(2)
+  tree.Insert(3)
+  tree.Insert(4)
+  tree.Insert(5)
+
+  if tree.Size() != 5 {
+    t.Errorf("Expected %d, got  %d\n", 1, tree.Size())
+  }
+
+  expectInOrder(&tree, []interface{}{4, 2 , 1, 3, 5}, t)
+  expectPreOrder(&tree, []interface{}{1, 2 , 4, 3, 5}, t)
+  expectPostOrder(&tree, []interface{}{4, 2 , 3, 5, 1}, t)
+}
+
+func expectInOrder(tree *trees.BinaryTree, expected []interface{}, t *testing.T){
+  i := 0
+  tree.InOrder(func(data interface{}) {
+    if expected[i] != data {
+      t.Errorf("Expected %d, got  %d\n", expected[i] , data)
+    }
+    i++
+  });
+}
+
+func expectPreOrder(tree *trees.BinaryTree, expected []interface{}, t *testing.T){
+  i := 0
+  tree.PreOrder(func(data interface{}) {
+    if expected[i] != data {
+      t.Errorf("Expected %d, got  %d\n", expected[i] , data)
+    }
+    i++
+  });
+}
+
+func expectPostOrder(tree *trees.BinaryTree, expected []interface{}, t *testing.T){
+  i := 0
+  tree.PosOrder(func(data interface{}) {
+    if expected[i] != data {
+      t.Errorf("Expected %d, got  %d\n", expected[i] , data)
+    }
+    i++
+  });
+}
+
