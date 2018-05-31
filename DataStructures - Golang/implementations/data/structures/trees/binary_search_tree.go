5bb914710d00e6ce640102588646aeec6bcc7d69
Alexander Soto <alexsotocx@gmail.com>
2018-05-31 19:17:30 +0200

    Find test

--- DataStructures - Golang/implementations/data/structures/trees/binary_search_tree.go
+++ DataStructures - Golang/implementations/data/structures/trees/binary_search_tree.go
@@ -13,7 +13,7 @@ func (tree *BinarySearchTree) Insert(data interfaces.Comparable) {
  } else {
    parent := tree.root
    for {
-     if data.Compare(parent.Data) == 1  {
+     if parent.Data.Compare(data) == 1  {
        if parent.RightChildren == nil {
          parent.RightChildren = newNode(data)
          break
@@ -28,4 +28,19 @@ func (tree *BinarySearchTree) Insert(data interfaces.Comparable) {
      }
    }
  }
+}
+
+func (tree *BinarySearchTree) Find(data interfaces.Comparable) (bool, interfaces.Comparable) {
+  current := tree.root
+  for current != nil {
+    valCom := current.Data.Compare(data)
+    if valCom == 0 {
+      return true, current.Data
+    } else if valCom == 1 {
+      current = current.RightChildren
+    } else {
+      current = current.LeftChildren
+    }
+  }
+  return false, nil
 }
--- DataStructures - Golang/implementations/test/binary_search_tree/binary_search_tree_test.go
+++ DataStructures - Golang/implementations/test/binary_search_tree/binary_search_tree_test.go
@@ -15,9 +15,10 @@ func (i Integer) Value() interface{} {
 }
 
 func (i Integer) Compare(a interfaces.Comparable) int8 {
-  if i.value == a.(Integer).value {
+  val := a.(Integer).value
+  if i.value == val {
     return 0
-  } else if  i.value > a.(Integer).value {
+  } else if  i.value < val {
     return 1
   }
   return -1
@@ -25,11 +26,11 @@ func (i Integer) Compare(a interfaces.Comparable) int8 {
 
 func TestInsert(t *testing.T) {
 	tree := trees.BinarySearchTree{}
-  tree.Insert(Integer{value: 4})
-  tree.Insert(Integer{value: 2})
-  tree.Insert(Integer{value: 5})
-  tree.Insert(Integer{value: 1})
-  tree.Insert(Integer{value: 3})
+ tree.Insert(Integer{value: 4})
+ tree.Insert(Integer{value: 2})
+ tree.Insert(Integer{value: 5})
+ tree.Insert(Integer{value: 1})
+ tree.Insert(Integer{value: 3})
 
 	if tree.Size() != 5 {
 		t.Errorf("Expected %d, got  %d\n", 1, tree.Size())
@@ -39,23 +40,23 @@ func TestInsert(t *testing.T) {
 	expectPreOrder(&tree, []interface{}{4, 2, 1, 3, 5}, t)
 	expectPostOrder(&tree, []interface{}{1, 3, 2, 5, 4}, t)
 }
-//
-//func TestFind(t *testing.T) {
-//	tree := trees.BinaryTree{}
-//	tree.Insert(1)
-//	tree.Insert(2)
-//	tree.Insert(3)
-//	tree.Insert(4)
-//	tree.Insert(5)
-//
-//	if found, c := tree.Find(5); !(found  && c == 5){
-//		t.Error("It should find the element 5")
-//	}
-//
-//	if found, c := tree.Find(6); found && c != nil {
-//		t.Error("It should not find the element 6")
-//	}
-//}
+
+func TestFind(t *testing.T) {
+	tree := trees.BinarySearchTree{}
+  tree.Insert(Integer{value: 4})
+  tree.Insert(Integer{value: 2})
+  tree.Insert(Integer{value: 5})
+  tree.Insert(Integer{value: 1})
+  tree.Insert(Integer{value: 3})
+
+	if found, c := tree.Find(Integer{value: 5}); !(found  && c.Value() == 5){
+		t.Error("It should find the element 5")
+	}
+
+	if found, c := tree.Find(Integer{value: 6}); found && c != nil {
+		t.Error("It should not find the element 6")
+	}
+}
 //
 //func TestDeleteRoot(t *testing.T) {
 //  tree := trees.BinaryTree{}
