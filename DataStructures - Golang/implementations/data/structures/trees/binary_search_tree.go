ae6605e1c552e069468eeaa611d91c5557f9448a
Alexander Soto <alexsotocx@gmail.com>
2018-06-02 20:59:49 +0200

    Binary Search tree => Delete function and test

--- DataStructures - Golang/implementations/data/structures/trees/binary_search_tree.go
+++ DataStructures - Golang/implementations/data/structures/trees/binary_search_tree.go
@@ -6,6 +6,10 @@ type BinarySearchTree struct {
   BinaryTree
 }
 
+func (n node) isLeaf() bool {
+  return n.RightChildren == nil && n.LeftChildren != nil
+}
+
 func (tree *BinarySearchTree) Insert(data interfaces.Comparable) {
  tree.size++
  if tree.root == nil {
@@ -30,17 +34,86 @@ func (tree *BinarySearchTree) Insert(data interfaces.Comparable) {
  }
 }
 
-func (tree *BinarySearchTree) Find(data interfaces.Comparable) (bool, interfaces.Comparable) {
+func (tree BinarySearchTree) Find(data interfaces.Comparable) (bool, interfaces.Comparable) {
+  _, nodeFound := tree.find(data)
+  if nodeFound == nil {
+    return false, nil
+  }
+  return true, nodeFound.Data
+}
+
+func (tree BinarySearchTree) find(data interfaces.Comparable) (*node, *node) {
   current := tree.root
+  var parent *node
   for current != nil {
     valCom := current.Data.Compare(data)
     if valCom == 0 {
-      return true, current.Data
-    } else if valCom == 1 {
+      return parent, current
+    }
+    parent = current
+    if valCom == 1 {
       current = current.RightChildren
     } else {
       current = current.LeftChildren
     }
   }
+  return nil, nil
+}
+
+
+func (tree *BinarySearchTree) Delete(data interfaces.Comparable) (bool, interfaces.Comparable) {
+  parent, toDelete := tree.find(data)
+  if toDelete != nil {
+    tree.size--
+    if toDelete.isLeaf() {
+      if toDelete == tree.root {
+        tree.root = nil
+        return true, data
+      }
+      if parent.LeftChildren == toDelete {
+        parent.LeftChildren = nil
+      } else {
+        parent.RightChildren = nil
+      }
+    } else  {
+      replacement := tree.findAndReplace(toDelete)
+      if toDelete.LeftChildren == nil {
+        if parent.RightChildren == toDelete {
+          parent.RightChildren = replacement
+        } else {
+          parent.LeftChildren = replacement
+        }
+      } else {
+        if toDelete.LeftChildren == replacement {
+          toDelete.Data = replacement.Data
+          toDelete.LeftChildren = replacement.LeftChildren
+        } else {
+          toDelete.Data = replacement.Data
+        }
+      }
+    }
+    return true, data
+  }
+
   return false, nil
+}
+
+
+func (tree BinarySearchTree) findAndReplace(current *node) (*node) {
+  if current.LeftChildren != nil {
+    parent := current
+    current = current.LeftChildren
+    for current.RightChildren != nil {
+      parent = current
+      current = current.RightChildren
+    }
+
+    if parent.RightChildren == current {
+      parent.RightChildren = current.LeftChildren
+      current.LeftChildren = nil
+    }
+    return current
+  }
+
+  return current.RightChildren
 }
--- DataStructures - Golang/implementations/test/binary_search_tree/binary_search_tree_test.go
+++ DataStructures - Golang/implementations/test/binary_search_tree/binary_search_tree_test.go
@@ -57,74 +57,78 @@ func TestFind(t *testing.T) {
 		t.Error("It should not find the element 6")
 	}
 }
+
+func TestDeleteRoot(t *testing.T) {
+ tree := trees.BinarySearchTree{}
+ tree.Insert(Integer{value: 4})
+ tree.Insert(Integer{value: 2})
+ tree.Insert(Integer{value: 5})
+ tree.Insert(Integer{value: 1})
+ tree.Insert(Integer{value: 3})
+ tree.Insert(Integer{value: 6})
+
+ d := Integer{value: 4}
+ if found, _ := tree.Find(d); !found {
+   t.Error("It should find the element 4")
+ }
+
+ tree.Delete(d)
+
+ if found, _ := tree.Find(d); found {
+   t.Error("It should not find the element 4")
+ }
+
+ expectInOrder(&tree, []interface{}{1, 2, 3, 5, 6}, t)
+}
 //
-//func TestDeleteRoot(t *testing.T) {
-//  tree := trees.BinaryTree{}
-//  tree.Insert(1)
-//  tree.Insert(2)
-//  tree.Insert(3)
-//  tree.Insert(4)
-//  tree.Insert(5)
-//  tree.Insert(6)
-//
-//  if found, _ := tree.Find(1); !found {
-//    t.Error("It should find the element 1")
-//  }
-//
-//  tree.Delete(1)
-//
-//  if found, _ := tree.Find(1); found {
-//    t.Error("It should not find the element 1")
-//  }
-//
-//  expectPreOrder(&tree, []interface{}{6, 2, 4, 3, 5}, t)
-//}
-//
-//func TestDeleteParentFromBranch(t *testing.T) {
-//  tree := trees.BinaryTree{}
-//  tree.Insert(1)
-//  tree.Insert(2)
-//  tree.Insert(3)
-//  tree.Insert(4)
-//  tree.Insert(5)
-//  tree.Insert(6)
-//
-//  if found, _ := tree.Find(2); !found {
-//    t.Error("It should find the element 2")
-//  }
-//
-//  tree.Delete(2)
-//
-//  if found, _ := tree.Find(2); found {
-//    t.Error("It should not find the element 2")
-//  }
-//
-//  expectPreOrder(&tree, []interface{}{1, 6, 4, 3, 5}, t)
-//}
-//
-//func TestDeleteLeft(t *testing.T) {
-//  tree := trees.BinaryTree{}
-//  tree.Insert(1)
-//  tree.Insert(2)
-//  tree.Insert(3)
-//  tree.Insert(4)
-//  tree.Insert(5)
-//  tree.Insert(6)
-//
-//  if found, _ := tree.Find(3); !found {
-//    t.Error("It should find the element 3")
-//  }
-//
-//  tree.Delete(3)
-//
-//  if found, _ := tree.Find(3); found {
-//    t.Error("It should not find the element 3")
-//  }
-//
-//  expectPreOrder(&tree, []interface{}{1, 2, 4, 6, 5}, t)
-//}
-//
-//
+func TestDeleteParentFromBranch(t *testing.T) {
+ tree := trees.BinarySearchTree{}
+ tree.Insert(Integer{value: 4})
+ tree.Insert(Integer{value: 2})
+ tree.Insert(Integer{value: 5})
+ tree.Insert(Integer{value: 1})
+ tree.Insert(Integer{value: 3})
+ tree.Insert(Integer{value: 6})
+
+ d := Integer{value: 2}
+
+ if found, _ := tree.Find(d); !found {
+   t.Error("It should find the element 2")
+ }
+
+ tree.Delete(d)
+
+ if found, _ := tree.Find(d); found {
+   t.Error("It should not find the element 2")
+ }
+
+ expectInOrder(&tree, []interface{}{1, 3, 4, 5, 6}, t)
+}
+
+func TestDeleteLeaf(t *testing.T) {
+ tree := trees.BinarySearchTree{}
+ tree.Insert(Integer{value: 4})
+ tree.Insert(Integer{value: 2})
+ tree.Insert(Integer{value: 5})
+ tree.Insert(Integer{value: 1})
+ tree.Insert(Integer{value: 3})
+ tree.Insert(Integer{value: 6})
+
+ d := Integer{value: 3}
+
+ if found, _ := tree.Find(d); !found {
+   t.Error("It should find the element 3")
+ }
+
+ tree.Delete(d)
+
+ if found, _ := tree.Find(d); found {
+   t.Error("It should not find the element 3")
+ }
+
+ expectInOrder(&tree, []interface{}{1, 2, 4, 5, 6}, t)
+}
+
 func expectInOrder(tree *trees.BinarySearchTree, expected []interface{}, t *testing.T) {
   i := 0
   tree.InOrder(func(data interfaces.Comparable) {
