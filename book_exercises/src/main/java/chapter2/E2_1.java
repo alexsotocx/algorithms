e3ca224d4219b59681269d9a1e3f67189ec887fe
Alexander Soto <alexsotocx@gmail.com>
2018-07-22 22:12:02 +0200

    Linked list exercises

---
+++ book_exercises/src/main/java/chapter2/E2_1.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+import datastructures.DoubleLinkedList;
+
+import java.util.HashSet;
+
+public class E2_1 {
+  DoubleLinkedList<Integer> removeDup(DoubleLinkedList<Integer> list) {
+    HashSet<Integer> set = new HashSet<>();
+    DoubleLinkedList<Integer>.Link<Integer> link = list.getFirst();
+    while(link != null) {
+      if(set.contains(link.data)) list.delete(link);
+      set.add(link.data);
+      link = link.next;
+    }
+    return list;
+  }
+}
