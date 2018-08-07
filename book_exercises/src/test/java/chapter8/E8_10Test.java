1ffd0266cd160233fe9faa3c3ede3a995959dd7e
Alexander Soto <alexsotocx@gmail.com>
2018-08-07 11:12:22 +0200

    Recursion and DP

---
+++ book_exercises/src/test/java/chapter8/E8_10Test.java
@@ -0,0 +1,22 @@
+package chapter8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class E8_10Test {
+
+  @Test
+  public void paint() {
+    char[][] grid = new char[][]{
+        "oowww".toCharArray(),
+        "cwwwo".toCharArray(),
+        "cgwww".toCharArray(),
+        "cwgbb".toCharArray(),
+        "ccccb".toCharArray()
+    };
+    E8_10 painter = new E8_10(grid);
+    assertEquals(9, painter.paint(0, 2, 'b'));
+    assertEquals(12, painter.paint(0, 2, 'x'));
+  }
+}
