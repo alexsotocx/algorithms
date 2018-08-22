2d2120db103a13253bf10128a7cdc6c4ea5ffa81
Alexander Soto <alexsotocx@gmail.com>
2018-08-22 22:20:36 +0200

    HashMaps

---
+++ online_judges/HackerRank/src/main/java/CountTriplets.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class CountTriplets {
+  long countTriplets(List<Long> arr, long r) {
+    HashMap<Long, Long> t1 = new HashMap<>();
+    HashMap<Long, Long> t2 = new HashMap<>();
+    long c = 0;
+    for(long v: arr) {
+      if(t2.containsKey(v)) c+= t2.get(v);
+      if(t1.containsKey(v)) t2.put(v*r, t2.getOrDefault(v*r, 0L) + t1.get(v));
+      t1.put(v*r, t1.getOrDefault(v*r, 0L) + 1);
+    }
+
+    return c;
+
+  }
+}
