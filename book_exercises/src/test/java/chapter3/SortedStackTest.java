8a8f5b0633be17683565bf3886e6938646ddf4a9
Alexander Soto <asoto@minddoc.de>
2020-04-16 06:50:32 +0200

    Sorted stack

---
+++ book_exercises/src/test/java/chapter3/SortedStackTest.java
@@ -0,0 +1,20 @@
+package chapter3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortedStackTest {
+  @Test
+  public void Test() {
+    SortedStack stack = new SortedStack();
+    stack.push(3);
+    stack.push(8);
+    stack.push(4);
+    stack.push(1);
+    stack.push(2);
+
+   assertEquals(Integer.valueOf(8), stack.peek());
+    for (int e : new int[]{8, 4, 3, 2, 1}) assertEquals(e, stack.pop());
+  }
+}
