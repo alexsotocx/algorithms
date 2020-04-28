baf3f8b123088dfd0b51432cc6dc82c392206747
Alexander Soto <asoto@minddoc.de>
2020-04-28 21:49:30 +0200

    refactor

--- book_exercises/src/main/java/chapter2/E2_6.java
+++ book_exercises/src/main/java/chapter2/E2_6.java
@@ -1,16 +1,16 @@
 package chapter2;
 
-import datastructures.LNode;
+import datastructures.LinkedListNode;
 
 public class E2_6 {
-  boolean isPalindrome(LNode head) {
-    LNode p1 = head, p2 = p1;
+  boolean isPalindrome(LinkedListNode head) {
+    LinkedListNode p1 = head, p2 = p1;
     if (p1 == null || p1.next == null) return true;
     if (p1.next.next == null && p1.value == p1.next.value) return true;
-    LNode stackp = null;
+    LinkedListNode stackp = null;
     int size = 0;
     while (p2 != null && p2.next != null) {
-      stackp = new LNode(p1.value, stackp);
+      stackp = new LinkedListNode(p1.value, stackp);
       p1 = p1.next;
       p2 = p2.next.next;
       size++;
