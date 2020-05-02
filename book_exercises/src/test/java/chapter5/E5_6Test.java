dc0b62980a49b9eb199cb37282e3db143b4559a9
Alexander Soto <asoto@minddoc.de>
2020-05-02 20:22:28 +0200

    book: improve Exercise 5.6

--- book_exercises/src/test/java/chapter5/E5_6Test.java
+++ book_exercises/src/test/java/chapter5/E5_6Test.java
@@ -12,5 +12,6 @@ public class E5_6Test {
     assertEquals(2, new E5_6().minConversion(15, 29));
     assertEquals(0, new E5_6().minConversion(15, 15));
     assertEquals(0, new E5_6().minConversion(0, 0));
+    assertEquals(1, new E5_6().minConversion(1, 0));
   }
 }
