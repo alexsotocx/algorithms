bc352f6eb6c404ae789ad25a1fe3ed3fc78fb03b
Alexander Soto <asoto@minddoc.de>
2020-05-15 07:38:49 +0200

    book: sorting

---
+++ book_exercises/src/main/java/chapter8/MagicIndex.java
@@ -0,0 +1,16 @@
+package chapter8;
+
+public class MagicIndex {
+  boolean hasMagicIndex(int[] arr) {
+    int low = 0;
+    int hi = arr.length - 1;
+    while(low != hi) {
+      int mid = (low + hi) / 2;
+      if (mid == arr[mid]) return true;
+      else if (mid < arr[mid]) hi = mid - 1;
+      else low = mid + 1;
+    }
+    if (arr[low] == low) return true;
+    return arr[hi] == hi;
+  }
+}
---
+++ book_exercises/src/test/java/chapter10/SortedMergeTest.java
@@ -0,0 +1,28 @@
+package chapter10;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedMergeTest {
+
+    @Test
+    void sort() {
+      int a[] = new int[]{2, 5, 10, 20, 0, 0, 0 };
+      int b[] = new int[]{1, 4, 10 };
+      new SortedMerge().sort(a, b, 3);
+      assertArrayEquals(new int[]{1, 2, 4, 5, 10, 10, 20 }, a);
+
+      a = new int[]{10, 11, 12, 20, 0, 0, 0 };
+      b = new int[]{1, 4, 10 };
+      new SortedMerge().sort(a, b, 3);
+      assertArrayEquals(new int[]{1, 4, 10, 10, 11, 12, 20 }, a);
+
+      a = new int[]{1, 4, 10, 0, 0, 0, 0 };
+      b = new int[]{20, 30, 40, 50  };
+      new SortedMerge().sort(a, b, 2);
+      assertArrayEquals(new int[]{1, 4, 10, 20, 30, 40, 50 }, a);
+    }
+}
