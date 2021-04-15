b389ced06b020b16b6ac39425f10736b9bbd35f5
Alexander Soto <asoto@minddoc.de>
2021-04-15 09:19:23 +0200

    chore: add more easy problems

---
+++ leetcode/src/easy/EmployeeImportance.java
@@ -0,0 +1,31 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class EmployeeImportance {
+  public int getImportance(List<Employee> employees, int id) {
+    HashMap<Integer, Employee> maps = new HashMap<>();
+    for(Employee x: employees) {
+      maps.put(x.id, x);
+    }
+    if(!maps.containsKey(id)) return 0;
+    return dfs(maps, id);
+  }
+
+  private int dfs(HashMap<Integer, Employee> maps, int start) {
+    Employee e = maps.get(start);
+    int sum = e.importance;
+    for(int i: e.subordinates) {
+      sum += dfs(maps, i);
+    }
+    return sum;
+  }
+}
+
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+
