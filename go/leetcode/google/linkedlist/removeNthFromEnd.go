7c0bc84c70bd12620627a88f597d487d35613e64
Alexander Soto <asoto@minddoc.de>
2021-09-07 21:49:06 +0200

    started linkedlist

---
+++ go/leetcode/google/linkedlist/removeNthFromEnd.go
@@ -0,0 +1,28 @@
+package google
+
+//  Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	p1 := head
+	p2 := head
+	for n > 0 {
+		p2 = p2.Next
+		n--
+	}
+	if p2 == nil {
+		return p1.Next
+	}
+	for p2.Next != nil {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+	if p1.Next != nil {
+		p1.Next = p1.Next.Next
+	}
+
+	return head
+}
