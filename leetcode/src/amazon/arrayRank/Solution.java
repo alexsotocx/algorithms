33890edd98c64bff7dbaec13cd0fed557d42c83f
Alexander Soto <asoto@minddoc.de>
2021-11-12 17:53:37 +0100

    fix

--- leetcode/src/amazon/arrayRank/Solution.java
+++ leetcode/src/amazon/arrayRank/Solution.java
@@ -1,15 +1,18 @@
 package amazon.arrayRank;
 
+
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.TreeSet;
 
 class Solution {
   public int[] arrayRankTransform(int[] arr) {
-    TreeSet<Integer> set = new TreeSet<Integer>();
+    int[] cp = Arrays.copyOf(arr, arr.length);
+    Arrays.sort(cp);
     HashMap<Integer, Integer> x = new HashMap<>();
-    for (int a : arr) set.add(a);
     int i = 1;
-    for (int a : set) x.put(a, i++);
+    for (int a : cp) {
+      if (!x.containsKey(a)) x.put(a, i++);
+    }
     int[] res = new int[arr.length];
     for (i = 0; i < arr.length; i++) {
       res[i] = x.get(arr[i]);
---
+++ leetcode/src/amazon/arrayRank/SolutionTest.java
@@ -0,0 +1,14 @@
+package amazon.arrayRank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+  @Test
+  void arrayRankTransform() {
+    int[] x = new int[]{40,10,20,30};
+    new Solution().arrayRankTransform(x);
+  }
+}
