9b7d4a7389875f0d37a72c2efd038783acaed241
Alexander Soto <alexsotocx@gmail.com>
2022-02-01 08:58:49 +0100

    improve total fruit version

--- leetcode/src/amazon/totalFruit/totalFruit.java
+++ leetcode/src/amazon/totalFruit/totalFruit.java
@@ -1,6 +1,7 @@
 package amazon.totalFruit;
 
-import java.util.*;
+
+import java.util.LinkedList;
 
 class Solution {
   private class Entry {
@@ -10,37 +11,19 @@ class Solution {
       this.f = f;
       this.p = p;
     }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Entry entry = (Entry) o;
-      return f == entry.f;
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(f);
-    }
   }
 
   public int totalFruit(final int[] fruits) {
     int max = 0;
     LinkedList<Entry> basketType = new LinkedList<>();
     basketType.add(new Entry(fruits[0], 0));
-    int prev = fruits[0];
     int windowStart = 0;
     for(int i = 0; i < fruits.length; i++){
-      Entry entry = new Entry(fruits[i], i);
-      boolean contained = basketType.contains(entry);
-      if(contained && prev == fruits[i]) continue;
-      basketType.remove(entry);
-      basketType.add(entry);
-      prev = fruits[i];
+      if(basketType.getLast().f == fruits[i]) continue;
+      if(basketType.getFirst().f == fruits[i]) basketType.removeFirst();
+      basketType.add(new Entry(fruits[i], i));
       if (basketType.size() == 3) {
-        Entry entryD = basketType.getFirst();
-        basketType.remove(entryD);
+        basketType.removeFirst();
         max = Math.max(max, i - windowStart);
         windowStart = basketType.getFirst().p;
       }
