f46f6ecb677e97012946aa59e3fd26030acb13d5
Alexander Soto <alexsotocx@gmail.com>
2022-03-12 19:05:43 +0100

    Improve

--- leetcode/src/explore/dp/jumpgame2/Solution.java
+++ leetcode/src/explore/dp/jumpgame2/Solution.java
@@ -4,15 +4,14 @@ import java.util.Arrays;
 
 class Solution {
   public int jump(int[] nums) {
-    int max = nums[0];
     int[] minSteps= new int[nums.length];
     Arrays.fill(minSteps, Integer.MAX_VALUE);
     minSteps[0] = 0;
 
     for(int i = 0; i < nums.length; i++) {
-      for (int j = 1; j <= nums[i] && j + i < nums.length; j++) {
-        if (minSteps[j+i] <= minSteps[i] + 1) continue;
-        minSteps[j+i] = minSteps[i] + 1;
+      for (int j = Math.min(nums.length - 1, i + nums[i]); j > i; j--) {
+        if (minSteps[j] <= minSteps[i] + 1) break;
+        minSteps[j] = minSteps[i] + 1;
       }
     }
     return minSteps[nums.length - 1];
