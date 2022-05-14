0acef0175469a8b8c5be84513c1306c4efcfd4e3
Alexander Soto <alexsotocx@users.noreply.github.com>
2022-05-14 18:58:40 +0200

    on the go

---
+++ leetcode/src/medium/powxn.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if (n < 0):
+        	return 1.0 / self.myFastPow(x, -n)
+        return self.myFastPow(x, n)
+  
+    def myFastPow(self, x: float, n: int) -> float:
+        if n == 0:
+        	return 1
+        if n == 1:
+        	return x
+
+        y = self.myPow(x, n //2)
+        if n %2 == 0:
+        	return y * y
+        return y * y * x
