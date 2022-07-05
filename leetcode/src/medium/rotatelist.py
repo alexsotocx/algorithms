2c555450983fd006eabc912520f339d784ebd2e0
Alexander Soto <alexsotocx@gmail.com>
2022-07-05 21:18:23 +0200

    Knapsack

--- leetcode/src/medium/rotatelist.py
+++ leetcode/src/medium/rotatelist.py
@@ -1,9 +1,10 @@
 # Definition for singly-linked list.
 class ListNode:
-	def __init__(self, val=0, next=None):
-		self.val = val
-		self.next = next
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 class Solution:
-	def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-		        
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+		
--- leetcode/src/medium/sqrt.py
+++ leetcode/src/medium/sqrt.py
@@ -1,19 +1,16 @@
-class Solution:	
-	def mySqrt(self, x: int) -> int:
-		lo = 0
-		hi = 1<<16
-		best = 1
-		while lo <= hi:
-			mid = (lo + hi) // 2
-			midv = mid * mid
-			if midv == x:
-				return mid
-			if midv > x:
-				hi = mid - 1
-			else:
-				best = mid
-				lo = mid + 1
-		return best
-        
-        		
-        	
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        lo = 0
+        hi = 1 << 16
+        best = 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            midv = mid * mid
+            if midv == x:
+                return mid
+            if midv > x:
+                hi = mid - 1
+            else:
+                best = mid
+                lo = mid + 1
+        return best
