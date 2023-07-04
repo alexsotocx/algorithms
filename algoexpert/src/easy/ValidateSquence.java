6d154165e5ce1c99eb56485e8d9fe81b8f02b4ef
Alexander Soto <alexsotocx@gmail.com>
2023-07-04 20:12:22 +0200

    Validate sequence

---
+++ algoexpert/src/easy/ValidateSquence.java
@@ -0,0 +1,21 @@
+package easy;
+
+import java.util.*;
+
+class Program {
+  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    int p1 = 0;
+    int aSize = array.size();
+    int sSize = sequence.size();
+    if (sSize > aSize) return false;
+
+    for (Integer integer : sequence) {
+      while (p1 < aSize && !array.get(p1).equals(integer)) p1++;
+      if (p1 == aSize) return false;
+      p1++;
+    }
+
+    return true;
+  }
+}
+
