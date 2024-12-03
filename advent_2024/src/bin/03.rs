e1679377aa14eda0a0633fd17accb5208c9b62c7
Alexander Soto <alexsotocx@gmail.com>
2024-12-03 08:15:14 +0100

    Apply github copilot suggestions

--- advent_2024/src/bin/03.rs
+++ advent_2024/src/bin/03.rs
@@ -13,65 +13,58 @@ xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 ";
 
 fn read_file<R: BufRead>(reader: R) -> Result<String, Box<dyn Error>> {
-    let mut l1 = String::new();
+    let mut content = String::new();
     for line in reader.lines() {
-        l1.push_str(&line?);
+        content.push_str(&line?);
     }
-    Ok(l1)
+    Ok(content)
 }
 
-
-fn part1(l1: String) -> i32 {
-    let re = Regex::new(r"mul\((\d+,\d+)\)").unwrap();
-    let mut res = 0;
-    for cap in re.captures_iter(&l1) {
-        let mut l2 = cap[1].split(",");
-        let a: i32 = l2.next().unwrap().parse().unwrap();
-        let b: i32 = l2.next().unwrap().parse().unwrap();
-        res += a * b;
-    }
-    return res;
+fn part1(input: &str) -> i32 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    re.captures_iter(input)
+        .map(|cap| {
+            let a: i32 = cap[1].parse().unwrap();
+            let b: i32 = cap[2].parse().unwrap();
+            a * b
+        })
+        .sum()
 }
 
-
-fn part2(l1: String) -> i32 {
+fn part2(input: &str) -> i32 {
     let re = Regex::new(r"do\(\)|don\'t\(\)").unwrap();
-    let mut res = 0;
+    let mut result = 0;
     let mut enabled = true;
-    let mut last_instruction_pos = 0;
-    for cap in re.captures_iter(&l1) {
+    let mut last_pos = 0;
+
+    for cap in re.captures_iter(input) {
         let instruction = &cap[0];
         let end = cap.get(0).unwrap().end();
+
         if enabled {
-            let l2 = &l1[last_instruction_pos..end];
-            res += part1(l2.to_string());
+            result += part1(&input[last_pos..end]);
         }
-        if instruction == "do()" {
-            enabled = true;
-        } else {
-            enabled = false;
-        }
-        last_instruction_pos = end;
+
+        enabled = instruction == "do()";
+        last_pos = end;
     }
 
     if enabled {
-        res += part1(l1[last_instruction_pos..].to_string());
+        result += part1(&input[last_pos..]);
     }
-    return res;
+
+    result
 }
+
 fn main() -> Result<(), Box<dyn Error>> {
-    // assert_eq!(161, part1(l1));
-
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    let l1 = read_file(input_file)?;
-    println!("Result part 1 = {}", part1(l1.clone()));
+    let input_content = read_file(input_file)?;
+    println!("Result part 1 = {}", part1(&input_content));
 
-
-    // part 2
     let test_data = BufReader::new(TEST.as_bytes());
-    let test = read_file(test_data)?;
-    assert_eq!(48, part2(test));
-    println!("Result part 2 = {}", part2(l1.clone()));
+    let test_content = read_file(test_data)?;
+    assert_eq!(48, part2(&test_content));
+    println!("Result part 2 = {}", part2(&input_content));
 
     Ok(())
 }
