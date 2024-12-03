88a2824a7577d6691f9996820be3726eeb0863d1
Alexander Soto <alexsotocx@gmail.com>
2024-12-03 07:24:34 +0100

    Part 2

--- advent_2024/src/bin/02.rs
+++ advent_2024/src/bin/02.rs
@@ -31,41 +31,43 @@ fn read_file<R: BufRead>(reader: R) -> Result<Vec<Vec<i32>>, Box<dyn Error>> {
     Ok(l1)
 }
 
+fn safe_list(l2: &Vec<i32>) -> i32 {
+    let increasing = l2.windows(2).all(|w| w[0] < w[1]);
+    let decreasing = l2.windows(2).all(|w| w[0] > w[1]);
+    let diff = l2
+        .windows(2)
+        .all(|w| (w[0] - w[1]).abs() >= 1 && (w[0] - w[1]).abs() <= 3);
+
+    if (increasing || decreasing) && diff {
+        return 1;
+    }
+    return 0;
+}
+
 fn part1(l1: &Vec<Vec<i32>>) -> i32 {
+    let mut res = 0;
+    for l2 in l1 {
+        res += safe_list(l2);
+    }
+    return res;
+}
+
+fn part2(l1: &Vec<Vec<i32>>) -> i32 {
     let mut res = 0;
 
     for l2 in l1 {
-        let increasing = l2.windows(2).all(|w| w[0] < w[1]);
-        let decreasing = l2.windows(2).all(|w| w[0] > w[1]);
-        let diff = l2
-            .windows(2)
-            .all(|w| (w[0] - w[1]).abs() >= 1 && (w[0] - w[1]).abs() <= 3);
-
-        if (increasing || decreasing) && diff {
-            res += 1;
+        for i in 0..l2.len() {
+            let mut l3 = l2.clone();
+            l3.remove(i);
+            if safe_list(&l3) == 1 {
+                res += 1;
+                break;
+            }
         }
     }
 
     return res;
 }
-// fn part2(l1: &Vec<Vec<i32>>) -> i32 {
-//     let mut res = 0;
-
-//     for l2 in l1 {
-//         let increasing = l2.windows(2).filter(|w| w[0] < w[1]).count();
-//         let decreasing = l2.windows(2).filter(|w| w[0] > w[1]).count();
-//         let diff = l2
-//             .windows(2)
-//             .filter(|w| (w[0] - w[1]).abs() >= 1 && (w[0] - w[1]).abs() <= 3).count();
-
-//         if (increasing || decreasing) && diff {
-//             res += 1;
-//         }
-//     }
-
-//     return res;
-// }
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     let test_data = BufReader::new(TEST.as_bytes());
@@ -75,7 +77,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let l1 = read_file(input_file)?;
     println!("Result part 1 = {}", part1(&l1));
-    // println!("Result part 2 = {}", part2(&l1, &l2));
+    println!("Result part 2 = {}", part2(&l1));
 
     Ok(())
 }
