b1f0514f7fefa9b8737230fd4f5002aad563280f
Alexander Soto <alexsotocx@gmail.com>
2024-12-07 13:48:50 +0100

    Refactor

--- advent_2024/src/bin/04.rs
+++ advent_2024/src/bin/04.rs
@@ -24,49 +24,46 @@ MXMXAXMASX
 ";
 
 fn read_file<R: BufRead>(reader: R) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut content = Vec::new();
-    for line in reader.lines() {
-        content.push(line.unwrap());
-    }
-    Ok(content)
+    reader.lines().collect::<Result<Vec<_>, _>>().map_err(Into::into)
 }
 
-fn valid_position(input: &Vec<String>, i: i32, j: i32) -> bool {
+fn valid_position(input: &[String], i: i32, j: i32) -> bool {
     i >= 0 && j >= 0 && i < input.len() as i32 && j < input[i as usize].len() as i32
 }
 
-fn safe_get(input: &Vec<String>, i: i32, j: i32) -> char {
+fn safe_get(input: &[String], i: i32, j: i32) -> char {
     if valid_position(input, i, j) {
-        return input[i as usize].chars().nth(j as usize).unwrap_or(' ');
+        input[i as usize].chars().nth(j as usize).unwrap_or(' ')
+    } else {
+        ' '
     }
-
-    return ' ';
 }
 
-fn check_horizontal(input: &Vec<String>, i: i32, j: i32) -> i32 {
-    let h1 = check_text_with_direction(input, i, j, TEXT, 0, 1).then(|| 1).unwrap_or(0);
-    let h2 = check_text_with_direction(input, i, j, TEXT, 0, -1).then(|| 1).unwrap_or(0);
+fn check_text_with_direction(input: &[String], i: i32, j: i32, text: &str, d1: i32, d2: i32) -> bool {
+    text.chars().enumerate().all(|(f, c)| safe_get(input, i + (f as i32) * d1, j + (f as i32) * d2) == c)
+}
 
+fn check_horizontal(input: &[String], i: i32, j: i32) -> i32 {
+    let h1 = check_text_with_direction(input, i, j, TEXT, 0, 1) as i32;
+    let h2 = check_text_with_direction(input, i, j, TEXT, 0, -1) as i32;
     h1 + h2
 }
 
-fn check_vertical(input: &Vec<String>, i: i32, j: i32) -> i32 {
-    let v1 = check_text_with_direction(input, i, j, TEXT, 1, 0).then(|| 1).unwrap_or(0);
-    let v2 = check_text_with_direction(input, i, j, TEXT, -1, 0).then(|| 1).unwrap_or(0);
-
+fn check_vertical(input: &[String], i: i32, j: i32) -> i32 {
+    let v1 = check_text_with_direction(input, i, j, TEXT, 1, 0) as i32;
+    let v2 = check_text_with_direction(input, i, j, TEXT, -1, 0) as i32;
     v1 + v2
 }
 
-fn check_diagonal(input: &Vec<String>, i: i32, j: i32) -> i32 {
-    let d1 = check_text_with_direction(input, i, j, TEXT, 1, 1).then(|| 1).unwrap_or(0);
-    let d2 = check_text_with_direction(input, i, j, TEXT, -1, -1).then(|| 1).unwrap_or(0);
-    let d3 = check_text_with_direction(input, i, j, TEXT, 1, -1).then(|| 1).unwrap_or(0);
-    let d4 = check_text_with_direction(input, i, j, TEXT, -1, 1).then(|| 1).unwrap_or(0);
-
+fn check_diagonal(input: &[String], i: i32, j: i32) -> i32 {
+    let d1 = check_text_with_direction(input, i, j, TEXT, 1, 1) as i32;
+    let d2 = check_text_with_direction(input, i, j, TEXT, -1, -1) as i32;
+    let d3 = check_text_with_direction(input, i, j, TEXT, 1, -1) as i32;
+    let d4 = check_text_with_direction(input, i, j, TEXT, -1, 1) as i32;
     d1 + d2 + d3 + d4
 }
 
-fn part1(input: &Vec<String>) -> i32 {
+fn part1(input: &[String]) -> i32 {
     let mut result = 0;
     for i in 0..input.len() as i32 {
         for j in 0..input[i as usize].len() as i32 {
@@ -78,20 +75,7 @@ fn part1(input: &Vec<String>) -> i32 {
     result
 }
 
-fn check_text_with_direction(
-    input: &Vec<String>,
-    i: i32,
-    j: i32,
-    text: &str,
-    d1: i32,
-    d2: i32,
-) -> bool {
-    (0..text.len()).all(|f: usize| {
-        safe_get(input, i + (f as i32) * d1, j + (f as i32) * d2) == text.chars().nth(f).unwrap()
-    })
-}
-
-fn check_xmas(input: &Vec<String>, i: i32, j: i32) -> i32 {
+fn check_xmas(input: &[String], i: i32, j: i32) -> i32 {
     let h1 = check_text_with_direction(input, i, j, TEXT1, 1, 1);
     let h2 = check_text_with_direction(input, i, j, TEXT2, 1, 1);
 
@@ -103,14 +87,14 @@ fn check_xmas(input: &Vec<String>, i: i32, j: i32) -> i32 {
             return 1;
         }
     }
-    return 0;
+    0
 }
 
-fn part2(input: &Vec<String>) -> i32 {
+fn part2(input: &[String]) -> i32 {
     let mut result = 0;
     for i in 0..input.len() as i32 {
         for j in 0..input[0].len() as i32 {
-            result += check_xmas(input, i, j)
+            result += check_xmas(input, i, j);
         }
     }
     result
