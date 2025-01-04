24d68c314af4e59317a5517317ce5a323b0ab4a9
Alexander Soto <alexsotocx@gmail.com>
2025-01-04 20:53:19 +0100

    Part 2

--- advent_2024/src/bin/12.rs
+++ advent_2024/src/bin/12.rs
@@ -20,7 +20,7 @@ MIIISIJEEE
 MMMISSJEEE
 ";
 
-const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (0, -1), (0, 1), (1, 0)];
+const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
 
 type TestCase = Vec<Vec<char>>;
 
@@ -33,44 +33,56 @@ fn read_file<R: BufRead>(reader: R) -> Result<TestCase, Box<dyn Error>> {
     Ok(result)
 }
 
-fn flood_filling(
-    i: i32,
-    j: i32,
-    test_case: &TestCase,
-    visited: &mut Vec<Vec<bool>>,
-) -> (i32, i32) {
-    let mut area = 1;
-    let mut permieter = 0;
-    visited[i as usize][j as usize] = true;
+fn inside(r: i32, c: i32, v: &TestCase) -> bool {
+    r >= 0 && r < v.len() as i32 && c >= 0 && c < v[r as usize].len() as i32
+}
 
-    for dir in DIRECTIONS {
-        let di = i + dir.0;
-        let dj = j + dir.1;
-        let inside =
-            di >= 0 && di < test_case.len() as i32 && dj >= 0 && dj < test_case[0].len() as i32;
-        if inside {
-            if test_case[i as usize][j as usize] != test_case[di as usize][dj as usize] {
-                permieter += 1;
-            } else if !visited[di as usize][dj as usize] {
-                let (r_area, r_permiter) = flood_filling(di, dj, test_case, visited);
-                area += r_area;
-                permieter += r_permiter;
-            }
-        } else {
-            permieter += 1;
+fn flood_filling(row: i32, col: i32, v: &TestCase, visited: &mut Vec<Vec<bool>>) -> (i32, i32) {
+    let mut area = 1;
+    let mut perimeter = 0;
+    visited[row as usize][col as usize] = true;
+
+    let valid = |p: (i32, i32)| -> bool {
+        let di = row + p.0;
+        let dj = col + p.1;
+
+        return inside(di, dj, v) && v[row as usize][col as usize] == v[di as usize][dj as usize];
+    };
+
+    for i in 0..4 {
+        let dir1 = DIRECTIONS[i];
+        let dir2 = DIRECTIONS[(i + 1) % 4];
+        let dir3 = (dir1.0 + dir2.0, dir1.1 + dir2.1);
+        if !valid(dir1) && !valid(dir2) {
+            perimeter += 1;
+        }
+        if valid(dir1) && valid(dir2) && !valid(dir3) {
+            perimeter += 1;
         }
     }
 
-    return (area, permieter);
+    for dir in DIRECTIONS {
+        let di = row + dir.0;
+        let dj = col + dir.1;
+
+        if valid(dir) && !visited[di as usize][dj as usize] {
+            let (a, p) = flood_filling(di, dj, v, visited);
+            area += a;
+            perimeter += p;
+        }
+    }
+
+    return (area, perimeter);
 }
 
-fn part1(test_case: &TestCase) -> i32 {
+fn part2(test_case: &TestCase) -> i32 {
     let mut visited = vec![vec![false; test_case[0].len()]; test_case.len()];
     let mut r = 0;
     for i in 0..test_case.len() {
         for j in 0..test_case[i].len() {
             if !visited[i][j] {
-                let (area, perimeter) = flood_filling(i as i32, j as i32, test_case, &mut visited);
+                let (area, perimeter) =
+                    flood_filling(i as i32, j as i32, test_case, &mut visited);
                 r += area * perimeter;
             }
         }
@@ -79,16 +91,15 @@ fn part1(test_case: &TestCase) -> i32 {
     return r;
 }
 
+
 fn main() -> Result<(), Box<dyn Error>> {
     let test_case = read_file(BufReader::new(TEST.as_bytes()))?;
-    assert_eq!(part1(&test_case), 1930);
+    assert_eq!(part2(&test_case), 1206);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let test_case = read_file(input_file)?;
-    println!("Result part 1 = {}", part1(&test_case));
-    // println!("Result part 2 = {}", part2(&test_case, 75));
+    println!("Result part 2 = {}", part2(&test_case));
     // println!("Result part 2 = {}", part2(&test_case));
 
     Ok(())
 }
-
